bef323a5e1f6015f481dd982dc01cc0e761b4295
yexiyue <yexiyue666@qq.com>
2025-07-21 16:57:04 +0800

    test: add test data

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const blogs = defineCollection({
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/blogs",
+  }),
+  schema: z.object({
+    title: z.optional(z.string()),
+    date: z.optional(z.date()),
+    tags: z.optional(z.array(z.string())),
+    categories: z.optional(z.array(z.string())),
+  }),
+});
+
+export const collections = {
+  blogs,
+};
