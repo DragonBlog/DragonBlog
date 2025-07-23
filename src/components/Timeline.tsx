ee208ff390f51caa37803f178d17507e339eaaa4
hhan <1994824227@qq.com>
2025-07-23 23:55:26 +0800

    ✨ feat: 优化Timeline组件demo

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -30,6 +30,12 @@ const formatDate = (date: Date | undefined) => {
 };
 
 const Timeline = ({ blogs }: TimelineProps) => {
+  const Blogs = [...blogs].sort((a, b) => {
+    if (!a.data.date) return 1;
+    if (!b.data.date) return -1;
+    return b.data.date.getTime() - a.data.date.getTime();
+  });
+
   const [scope, animate] = useAnimate();
   const ref = useRef(null);
   const isInView = useInView(ref);
@@ -42,15 +48,25 @@ const Timeline = ({ blogs }: TimelineProps) => {
 
   return (
     <motion.ul ref={scope}>
-      {blogs.map((blog, index) => (
+      {Blogs.map((blog, index) => (
         <motion.li
           key={blog.id}
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: index * 0.15 }}
-          className="text-red-500 text-xl  px-4 py-2  "
+          className="text-sm px-4 py-2"
         >
-          {blog.data.title} ----------- {formatDate(blog.data.date)}
+          <div className="flex items-center w-1/2">
+            <div className="w-2 h-2 bg-blue-500 rounded-full mr-2"></div>
+
+            <div className="flex-1 flex justify-start">
+              <div className="cursor-pointer relative group text-left">
+                {blog.data.title}
+                <div className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></div>
+              </div>
+            </div>
+            <div className="pl-6 min-w-max"> {formatDate(blog.data.date)}</div>
+          </div>
         </motion.li>
       ))}
     </motion.ul>
