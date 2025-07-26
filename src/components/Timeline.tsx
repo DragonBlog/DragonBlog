4ceff4cd1a6e2a13fe0aa2edaab968cfd1b7c7c9
hhan <1994824227@qq.com>
2025-07-26 23:44:17 +0800

    ✨ feat: 添加 radash 依赖并优化时间线组件，按年份分组博客

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import { motion, useAnimate, useInView } from "motion/react";
-
+import dayjs from "dayjs";
+import { group, sort } from "radash";
+import { div } from "motion/react-client";
 type Blog = {
   id: string;
   body?: string;
@@ -18,23 +20,32 @@ type Blog = {
 interface TimelineProps {
   blogs: Blog[];
 }
+const formatData = (blogs: Blog[]) => {
+  const sortBlogs = blogs.map((blog) => {
+    const date = dayjs(blog.data.date);
+    return {
+      ...blog,
+      date: date.format("MM/DD/YYYY"),
+      year: Number(date.format("YYYY")),
+      sortDate: date.unix(),
+    };
+  });
 
-const formatDate = (date: Date | undefined) => {
-  if (!date) return "No date available";
+  const grouped = group(sortBlogs, (blog) => blog.year);
 
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = date.getDate().toString().padStart(2, "0");
-  const year = date.getFullYear();
+  for (const year in grouped) {
+    //非空断言 ！
+    grouped[year] = sort(grouped[year]!, (b) => b.sortDate).reverse();
+  }
 
-  return `${month}/${day}/${year}`;
+  return grouped;
 };
 
 const Timeline = ({ blogs }: TimelineProps) => {
-  const Blogs = [...blogs].sort((a, b) => {
-    if (!a.data.date) return 1;
-    if (!b.data.date) return -1;
-    return b.data.date.getTime() - a.data.date.getTime();
-  });
+  const groupedBlogs = formatData(blogs);
+
+  //sort 只对数字排序
+  const years = sort(Object.keys(groupedBlogs), (year) => Number(year), true);
 
   const [scope, animate] = useAnimate();
   const ref = useRef(null);
@@ -44,29 +55,39 @@ const Timeline = ({ blogs }: TimelineProps) => {
     if (isInView) {
       animate("li", { opacity: 1, y: 0 });
     }
-  }, [isInView]);
+  }, [isInView, animate]);
 
   return (
     <motion.ul ref={scope}>
-      {Blogs.map((blog, index) => (
-        <motion.li
-          key={blog.id}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: index * 0.15 }}
-          className="text-sm px-4 py-2"
-        >
-          <div className="flex items-center w-1/2">
-            <div className="w-2 h-2 bg-blue-500 rounded-full mr-2"></div>
-
-            <div className="flex-1 flex justify-start">
-              <div className="cursor-pointer relative group text-left">
-                {blog.data.title}
-                <div className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></div>
-              </div>
-            </div>
-            <div className="pl-6 min-w-max"> {formatDate(blog.data.date)}</div>
+      {years.map((year) => (
+        <motion.li key={year} className="mb-6">
+          <div className="text-lg px-4 mb-2">
+            {year}
+            <span className="text-sm pl-2">
+              ({groupedBlogs[Number(year)]?.length || 0})
+            </span>
           </div>
+          <ul>
+            {groupedBlogs[Number(year)]?.map((blog, index) => (
+              <motion.li
+                key={blog.id}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: index * 0.15 }}
+                className="text-sm px-4 py-2"
+              >
+                <div className="flex items-center w-1/2">
+                  <div className="w-2 h-2 bg-blue-500 rounded-full mr-2"></div>
+                  <div className="flex-1 flex justify-start">
+                    <div className="cursor-pointer relative group text-left hover:text-blue-500">
+                      {blog.data.title}
+                      <div className="absolute left-0 bottom-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></div>
+                    </div>
+                  </div>
+                </div>
+              </motion.li>
+            ))}
+          </ul>
         </motion.li>
       ))}
     </motion.ul>
